3fda6142b4d02f62ddf49dd8470493db63ca7ea5
Luca Garbelli <lu.gbelli@hotmail.it>
2017-02-17 19:25:57 +0100

    - aggiunta classe DamageablesItems (da completare)

---
+++ Assets/Scripts/DamageablesItems.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// #################################################################### DA NON CANCELLARE ! ###############################################################################
+public class DamageablesItems {
+
+    public DamageablesItems()
+    {
+
+    }
+
+    void LoadPrefab()
+    {
+        GameObject[] prefabs = Resources.LoadAll<GameObject>("/Prefabs/Agents");
+        Debug.Log(prefabs.Length);
+    }
+}

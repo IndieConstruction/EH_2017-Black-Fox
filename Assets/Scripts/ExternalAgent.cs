811bfa09584e8e1c6db49ad796aaf59474e6aacb
Luca <lu.gbelli@hotmail.it>
2017-02-23 17:43:56 +0100

    - aggiunto funzione base dell'agente esterno

--- Assets/Scripts/ExternalAgent.cs
+++ Assets/Scripts/ExternalAgent.cs
@@ -5,21 +5,45 @@ using UnityEngine;
 
 public class ExternalAgent : MonoBehaviour, IDamageable {
 
-    void Start ()
+    Transform target;
+    public float life;
+    public float velocity;
+
+    public float Life
     {
-		
-	}
-	
+        get { return life; }
+        set { life = value; }
+    }
 	void Update ()
     {
-		
-	}
+        MoveTowards();
+    }
+
+    void MoveTowards()
+    {
+        GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * velocity, ForceMode.Force);
+    }
+
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Wall")
+            GetComponent<Collider>().isTrigger = false;
+    }
 
     #region Interface
 
     public void Damage(float _damage)
     {
-        throw new NotImplementedException();
+        Life -= _damage;
+        if (Life < 1)
+        {
+            Destroy(gameObject);
+        }
     }
 
     #endregion

021028e301a0376ad982ccffa660b019c47baba0
FlyingBoar <montecristo310@gmail.com>
2017-03-01 11:33:21 +0100

    - Aggiunto elemento livello "Freccia" con relativo script

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+    public float Force = 100000;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<Agent>() != null)
+        {
+            Debug.Log("collisione");
+            other.GetComponent<Rigidbody>().AddForce(transform.forward * Force, ForceMode.Force);
+            other.GetComponent<PlacePin>().enabled = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Agent>() != null)
+        {
+            other.GetComponent<PlacePin>().enabled = true;
+        }
+    }
+}

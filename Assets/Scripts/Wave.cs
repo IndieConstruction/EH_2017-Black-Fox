aa0323aed1f41864c89200b267ee6d64b5c2b147
Luca Garbelli <lu.gbelli@hotmail.it>
2017-03-03 12:48:41 +0100

    - Inizo creazione onda

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+    public float velocity;
+
+
+    void Start ()
+    {
+		
+	}
+	
+
+	void FixedUpdate ()
+    {
+        MoveForward();
+    }
+
+    void MoveForward()
+    {
+        transform.Translate(Vector3.forward * velocity);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Wall")
+        {
+            Debug.Log("kill");
+            Destroy(gameObject);
+        }
+            
+    }
+}

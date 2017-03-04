73a41d1d8b496578a18026960bd545487a34bf91
AlbeGame <alberto.paparella.88@gmail.com>
2017-03-04 11:16:09 +0100

    Versione stabile. RopeController v.1.2

--- Assets/Scripts/RopeController.cs
+++ Assets/Scripts/RopeController.cs
@@ -4,19 +4,20 @@ using System.Collections.Generic;
 
 public class RopeController : MonoBehaviour
 {
-	public GameObject FragmentPrefab;
-    public Transform AnchorPoint;	
-	public int MaxLength = 80;
-    [Range(.0f,1f)]public float DensityOfFragments = .1f;
+    public GameObject FragmentPrefab;
+    public Transform AnchorPoint;
+    public int MaxLength = 80;
+    [Range(.0f, 1f)]
+    public float DensityOfFragments = .1f;
 
     List<GameObject> fragments = new List<GameObject>();
     LineRenderer lineRend;
     float ropeWidth;
     Vector3 offSet;
 
-	float fragmentDistance;
-	
-	void Start()
+    float fragmentDistance;
+
+    void Start()
     {
         lineRend = GetComponent<LineRenderer>();
         ropeWidth = GetComponent<LineRenderer>().widthMultiplier;
@@ -56,21 +57,22 @@ public class RopeController : MonoBehaviour
         //Prevent to extend the rope over MaxLength
         if (fragments.Count >= MaxLength)
             return;
-        
+
         //Keep building the rope until the AnchorPoint ore the MaxLength are reached
-        for (int i = fragments.Count; i <MaxLength; i++)
+        for (int i = fragments.Count; i < MaxLength; i++)
         {
             //Add a new Fragment to the rope
             position = fragments[i - 1].transform.position + offSet;
-            GameObject newFragment = Instantiate(FragmentPrefab, position, fragments[i-1].transform.rotation);
+            GameObject newFragment = Instantiate(FragmentPrefab, position, Quaternion.LookRotation(position));
             fragments.Add(newFragment);
             newFragment.transform.parent = fragments[0].transform;
             newFragment.name = "Fragment_" + i;
             //Collider configuration
-            collider = newFragment.GetComponent<CapsuleCollider>();
-            collider.radius = ropeWidth/2;
-            collider.center = offSet/2;
-            collider.height = fragmentDistance;
+            collider = newFragment.GetComponentInChildren<CapsuleCollider>();
+            collider.radius = ropeWidth / 2;
+            collider.center = Vector3.forward * fragmentDistance / 2;
+            collider.height = fragmentDistance + collider.radius;
+            collider.GetComponent<RopeForcedLook>().Target = fragments[i - 1];
             //Joint Configuration
             joint = newFragment.GetComponent<ConfigurableJoint>();
             joint.connectedBody = fragments[i - 1].GetComponent<Rigidbody>();
@@ -84,13 +86,13 @@ public class RopeController : MonoBehaviour
                 joint.autoConfigureConnectedAnchor = false;
                 joint.connectedAnchor = offSet;
                 break;
-            }                    
+            }
         }
 
-        if(AnchorPoint.GetComponent<ConfigurableJoint>().connectedBody == null)
+        if (AnchorPoint.GetComponent<ConfigurableJoint>().connectedBody == null)
         {
             AnchorPoint.GetComponent<ConfigurableJoint>().connectedBody = fragments[fragments.Count - 1].GetComponent<Rigidbody>();
-            Debug.Log("WARNING: MaxLength not enough to reach " + AnchorPoint.name + " in " +AnchorPoint.position);
+            Debug.Log("WARNING: MaxLength not enough to reach " + AnchorPoint.name + " in " + AnchorPoint.position);
             int fragmentsNeeded = (int)(Vector3.Distance(AnchorPoint.position, fragments[fragments.Count - 1].transform.position) / offSet.magnitude);
             Debug.Log(fragmentsNeeded + MaxLength + " needed");
         }
@@ -108,10 +110,10 @@ public class RopeController : MonoBehaviour
         Vector3 dir = AnchorPoint.position - _origin.position;
         dir = dir.normalized;
 
-        if(fragments.Count <= 1)
+        if (fragments.Count <= 1)
         {
             //Measure OffSet magnitude only if first time run of this method
-            fragmentDistance = Vector3.Distance(AnchorPoint.position, _origin.position) / DensityOfFragments/100;     
+            fragmentDistance = Vector3.Distance(AnchorPoint.position, _origin.position) / DensityOfFragments / 100;
         }
         return dir * fragmentDistance;
     }
@@ -123,7 +125,7 @@ public class RopeController : MonoBehaviour
     public void ExtendRope()
     {
         AnchorPoint.GetComponent<ConfigurableJoint>().connectedBody = null;
-        BuildRope(fragments[fragments.Count-1]);
+        BuildRope(fragments[fragments.Count - 1]);
     }
     #endregion
 }

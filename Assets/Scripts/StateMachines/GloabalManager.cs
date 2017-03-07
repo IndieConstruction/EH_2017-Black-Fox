7533f7332af3f2d470210b2fe5a7ed3f13841a70
Luca Garbelli <lu.gbelli@hotmail.it>
2017-03-07 11:09:42 +0100

    - inizio state machine

---
+++ Assets/Scripts/StateMachines/GloabalManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackFox
+{
+    public class GloabalManager : BaseStateMachine
+    {
+
+
+        void Start()
+        {
+            CurrentState = new MainMenuState();
+        }
+
+
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/States/MainMenuState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace BlackFox
+{
+    public class MainMenuState : StateBase
+    {
+
+        public override void OnStart()
+        {
+
+        }
+
+        public override void OnUpdate()
+        {
+
+        }
+
+        public override void OnEnd()
+        {
+
+        }
+    }
+}

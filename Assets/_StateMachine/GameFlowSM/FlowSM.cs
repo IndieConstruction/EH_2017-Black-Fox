315a9d1e6c1e174ee64e8082ffddabb31e6bd62a
Brago <brago@indieconstruction.com>
2017-03-09 14:29:26 +0100

    Creato nuovo branch per refactoring StateMachine.

--- Assets/_StateMachine/GameFlowSM/FlowSM.cs
+++ Assets/_StateMachine/GameFlowSM/FlowSM.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// State machine che gestisce il flow generale dell'applicazione.
+/// </summary>
 public class FlowSM : StateMachineBase {
 
     private void Start() {

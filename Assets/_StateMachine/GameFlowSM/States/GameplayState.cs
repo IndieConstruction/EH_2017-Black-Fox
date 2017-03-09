0131ca47089ac0d096f451cdd535764c94c58305
Brago <brago@indieconstruction.com>
2017-03-09 14:38:25 +0100

    -

--- Assets/_StateMachine/GameFlowSM/States/GameplayState.cs
+++ Assets/_StateMachine/GameFlowSM/States/GameplayState.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+/// <summary>
+/// Reppresenta lo stato di gameplay della flow state machine.
+/// </summary>
 public class GameplayState : StateBase {
 
     public override void OnStart() {

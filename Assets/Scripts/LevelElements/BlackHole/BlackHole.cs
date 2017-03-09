90bd5f87076e6e6805a11882021364b78b9f2662
FlyingBoar <montecristo310@gmail.com>
2017-03-09 12:55:23 +0100

    - Aggiunta la funzionalità del buco nero per distruggere l'agente quando supera una determinata soglia

--- Assets/Scripts/LevelElements/BlackHole/BlackHole.cs
+++ Assets/Scripts/LevelElements/BlackHole/BlackHole.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlackHole : MonoBehaviour {
+namespace BlackFox
+{
+    public class BlackHole : MonoBehaviour
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        public float Range;
+
+        // la velocità con cui viene spostato verso il centro
+        public float Attraction = 15;
+
+        private void OnTriggerStay(Collider other)
+        {
+            GameObject tempObj = other.gameObject;
+            
+            
+
+            if (tempObj.GetComponent<Agent>() != null)
+            {
+                tempObj.transform.position = Vector3.MoveTowards(tempObj.transform.position, transform.localPosition, Attraction);
+                if (Vector3.Distance(tempObj.transform.position, transform.position) <= Range)
+                {
+                    Destroy(tempObj);
+                }
+                
+                /*Vector3 Distance = tempObj.transform.position - transform.position;
+                Distance.z = 0;
+
+                float SquareOfDistance = Distance.sqrMagnitude;
+
+                if(SquareOfDistance > 0.0001f)
+                {
+                    //tempObj.GetComponent<Rigidbody>().AddForce(new Vector3(transform.position.x, transform.position.y, transform.position.z), ForceMode.Acceleration);
+                }*/
+            }
+
+        }
+    }
 }

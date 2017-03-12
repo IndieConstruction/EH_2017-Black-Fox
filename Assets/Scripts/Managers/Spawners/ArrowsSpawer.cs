d0751974d31570cc5c16447b3cac3f0836f3c509
AlbeGame <alberto.paparella.88@gmail.com>
2017-03-12 23:57:40 +0100

    Creato ArrowSpawner (da testare)

--- Assets/Scripts/Managers/Spawners/ArrowsSpawer.cs
+++ Assets/Scripts/Managers/Spawners/ArrowsSpawer.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 namespace BlackFox
 {
+    /// <summary>
+    /// Spawn a random Setup between 1 and the higher IDSetup value
+    /// </summary>
     public class ArrowsSpawer : SpawnerBase
     {
         Arrow[] arrows;
+        int maxRandSetup = 0;
+        int randomSetup = 1;
 
         protected override void OnActivation()
-        {
+        {            
+            if (arrows.Length == 0)
+            {
+                //Get reference first time
+                arrows = FindObjectsOfType<Arrow>();
+                //Find the higher IDSetup value
+                foreach (Arrow arr in arrows)
+                    if (maxRandSetup < arr.IDSetup)
+                        maxRandSetup = arr.IDSetup;
+            }
+            //Pick a random Setup
+            randomSetup = Random.Range(1, maxRandSetup);
+            //Deactivate all the inactive Setups
+            foreach (Arrow arr in arrows)
+                if (arr.IDSetup != randomSetup)
+                    arr.gameObject.SetActive(false);
+        }
 
+        protected override void OnDeactivation()
+        {   
+            //Reactivate the deactivated Setups
+            foreach (Arrow arr in arrows)
+                if (arr.IDSetup != randomSetup)
+                    arr.gameObject.SetActive(true);
         }
     }
 }

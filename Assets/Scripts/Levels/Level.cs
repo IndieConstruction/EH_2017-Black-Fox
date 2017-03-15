e56d92d8665143f5b6dc6856c50650570fb19527
Brago <brago@indieconstruction.com>
2017-03-15 18:01:42 +0100

    Iniziata versione scriptable object per Livels

---
+++ Assets/Scripts/Levels/Level.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackFox {
+    [CreateAssetMenu(fileName = "LevelName", menuName = "Levels/NewLevel", order = 1)]
+    public class Level : ScriptableObject {
+
+        public string LevelName;
+        public GameObject ArenaPrefab;
+        public List<SpawnerBase> Spawners = new List<SpawnerBase>();
+    } 
+}

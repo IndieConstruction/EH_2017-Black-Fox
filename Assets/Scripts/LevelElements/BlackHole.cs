8443997a58c101a713546a7a9ea353f1b1044874
FlyingBoar <montecristo310@gmail.com>
2017-03-19 23:43:49 +0100

    - Modificato lo script BlackHole, prima di apparire* controlla se è vicino un muro o al core. (*attivre il meshrenderer)

--- Assets/Scripts/LevelElements/BlackHole.cs
+++ Assets/Scripts/LevelElements/BlackHole.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace BlackFox
 {
+    /// IMPORTANTE IL MESH RENDERER DEL GAMEOBJECT A CUI E' ATTACCATO DEVE ESSERE DISATTIVATO \\\
     public class BlackHole : MonoBehaviour
     {
 
@@ -11,21 +12,52 @@ namespace BlackFox
         public float RangeToDestroyPins;
         public int Size = 1;
         float Timer = 5;
-
+        States currentState;
+        float CheckTimer = 1f;
         // la velocità con cui viene spostato verso il centro
         public float Attraction = 15;
 
-        private void Update()
+        enum States
         {
-            ChangeSize(Size);
+            CheckZoneOfSpawn,
+            Active,
         }
 
+        private void Start()
+        {
+            currentState = States.CheckZoneOfSpawn;
+        }
+
+        private void Update()
+        {
+            switch (currentState)
+            {
+                case States.CheckZoneOfSpawn:
+                    CheckTimer -= Time.deltaTime;
+                    if (CheckTimer <= 0)
+                    {
+                        GetComponent<MeshRenderer>().enabled = true;
+                        currentState = States.Active;
+                    }
+                    break;
+                case States.Active:
+                        ChangeSize(Size);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Ad ogni dimensione del buco nero, applica una forza di attrazione differente.
+        /// </summary>
+        /// <param name="_size">La dimensione del buco nero</param>
         void ChangeSize(int _size)
         {
             switch (_size)
             {
                 case 1:
-                    Attraction = 12;
+                    Attraction = 0.1f;
                     Timer -= Time.deltaTime;
                     if (Timer <= 0)
                     {
@@ -35,7 +67,7 @@ namespace BlackFox
                     }
                     break;
                 case 2:
-                    Attraction = 15;
+                    Attraction = 0.3f;
                     Timer -= Time.deltaTime;
                     if (Timer <= 0)
                     {
@@ -45,7 +77,7 @@ namespace BlackFox
                     }
                     break;
                 case 3:
-                    Attraction = 18;
+                    Attraction = 0.7f;
                     Timer -= Time.deltaTime;
                     if (Timer <= 0)
                     {

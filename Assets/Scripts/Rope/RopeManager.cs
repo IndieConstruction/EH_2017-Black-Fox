d620a8cf39d9dd7f3ff0cb2330d397ffc7db9f11
AlbeGame <alberto.paparella.88@gmail.com>
2017-03-24 10:35:36 +0100

    -Aggiornamento Plugins

---
+++ Assets/Scripts/Rope/RopeManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rope
+{
+    public class RopeManager : MonoBehaviour
+    {
+        public List<RopeController> RopeControllers = new List<RopeController>(4);
+
+        LineRenderer[] renderers;
+
+    }
+}

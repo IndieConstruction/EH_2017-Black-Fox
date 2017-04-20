bec7d4fefbe3a1e06b8d8488c2597328baf7a683
AlbeGame <alberto.paparella.88@gmail.com>
2017-04-20 12:10:44 +0200

    Sistemato lo scriptable object del modello

--- Assets/Scripts/Avatar/ShipModel.cs
+++ Assets/Scripts/Avatar/ShipModel.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace BlackFox
 {
+    [CreateAssetMenu(fileName = "ShipModel", menuName = "Ships/NewShip", order = 1)]
     public class ShipModel : ScriptableObject
     {         
         public Mesh mesh;

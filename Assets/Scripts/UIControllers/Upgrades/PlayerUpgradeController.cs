73d756c2da575e7d04b8761c1da94f88fb395d4c
FlyingBoar <montecristo310@gmail.com>
2017-05-06 11:19:45 +0200

    - Bozza Upgrade Menu (Da completare e rivedere)

---
+++ Assets/Scripts/UIControllers/Upgrades/PlayerUpgradeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackFox
+{
+
+    public class PlayerUpgradeController : BaseMenu
+    {
+        public UpgradeControllerID MenuID;
+
+        private Avatar avatar;
+
+        public Avatar Avatar
+        {
+            get { return avatar; }
+            set { avatar = value; }
+        }
+
+        List<IUpgrade> upgradeItem = new List<IUpgrade>();
+
+        public void OnStart()
+        {
+            foreach (IUpgrade item in GetComponentsInChildren<IUpgrade>())
+            {
+                item.PlayerUpgradeController = this;
+            }
+            //FindISelectableChildren();
+        }
+
+        //TODO: completare facendo da intermediario fra gli IUpgrade e l'avatar
+        public void ApplyUpgrade(IUpgrade _upgrade)
+        {
+            
+        }
+
+    }
+
+    /// <summary>
+    /// Serve per associare al upgrade controller il player corrispondente
+    /// </summary>
+    public enum UpgradeControllerID
+    {
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+    }
+}

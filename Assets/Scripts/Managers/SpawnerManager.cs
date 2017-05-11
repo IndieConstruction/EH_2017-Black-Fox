c8f9f4ef25b58039abb661a20bc727bca5ac9479
Brago <brago@indieconstruction.com>
2017-05-11 17:30:21 +0200

    - Eliminazione spawner nulli prima dello start

--- Assets/Scripts/Managers/SpawnerManager.cs
+++ Assets/Scripts/Managers/SpawnerManager.cs
@@ -14,20 +14,21 @@ namespace BlackFox
         #region API
         public void InstantiateNewSpawners(Level _currentLevel)
         {
-            //Spawners.Add(_currentLevel.ArrowsSpawner.CreateInstance(_currentLevel.ArrowsSpawner, transform));
-            //Spawners.Add( _currentLevel.BlackHoleSpawner.CreateInstance(_currentLevel.BlackHoleSpawner, transform));
+            Spawners.Add(_currentLevel.ArrowsSpawner.CreateInstance(_currentLevel.ArrowsSpawner, transform));
+            Spawners.Add(_currentLevel.BlackHoleSpawner.CreateInstance(_currentLevel.BlackHoleSpawner, transform));
             Spawners.Add(_currentLevel.ExternalElementSpawner.CreateInstance(_currentLevel.ExternalElementSpawner, transform));
-            //Spawners.Add( _currentLevel.TurretSpawner.CreateInstance(_currentLevel.TurretSpawner, transform));
-            //Spawners.Add(_currentLevel.WaveSpawner.CreateInstance(_currentLevel.WaveSpawner, transform));
+            Spawners.Add(_currentLevel.TurretSpawner.CreateInstance(_currentLevel.TurretSpawner, transform));
+            Spawners.Add(_currentLevel.WaveSpawner.CreateInstance(_currentLevel.WaveSpawner, transform));
 
-            // TODO : eliminarre i null dalla lista
+            Spawners.RemoveAll(s => s == null);
         }      
 
         public void InitSpawners()
         {
             foreach (SpawnerBase spawner in Spawners)
             {
-                spawner.Init();
+                if(spawner != null)
+                    spawner.Init();
             }
         }
         #endregion

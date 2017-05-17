178764e86125893bdf101cbfac9c59ed0f577031
FlyingBoar <montecristo310@gmail.com>
2017-05-17 13:00:07 +0200

    - Creato PowerUp Piazza pulita

---
+++ Assets/Scripts/PowerUps/CleanSweep/CleanSweep.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackFox
+{
+
+    public class CleanSweep : PowerUpBase
+    {
+        public Cleaner Cleaner;
+
+        
+        void Start()
+        {
+            DestroyAfterUse = false;
+        }
+
+        public override void UsePowerUp()
+        {
+            LifeTime = 5;
+            Cleaner.GetComponent<SphereCollider>().enabled = true;
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUps/CleanSweep/Cleaner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace BlackFox
+{
+
+    public class Cleaner : MonoBehaviour
+    {
+        Tweener AnimationTween;
+
+        
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer == 9)
+                Destroy(other.gameObject);
+        }
+
+    }
+}

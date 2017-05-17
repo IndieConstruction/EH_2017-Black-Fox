58f8afc8f980e610b5c767a20ddda4502dfa5120
Luca Garbelli <lu.gbelli@hotmail.it>
2017-05-17 12:49:21 +0200

    - aggiunti constrain al data manager

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -13,9 +13,56 @@ namespace BlackFox
             private set { _avatarDatas = value; }
         }
 
+        List<ModelConstrain> modelConstrains = new List<ModelConstrain>();
+
         public void Init()
         {
             InstantiateAvatarDatas();
+            InitConstrains(GameManager.Instance.PlayerMng.Players);
+        }
+
+        public void SetConstrainsAvatar(Player _player, AvatarData _data)
+        {
+            for (int i = 0; i < modelConstrains.Count; i++)
+            {
+                if (modelConstrains[i].player == _player)
+                    modelConstrains[i].avatarData = AvatarDatas[i];
+            }
+        }
+
+        public void SetConstrainsColor(Player _player, ColorSetData _color)
+        {
+            for (int i = 0; i < modelConstrains.Count; i++)
+            {
+                if (modelConstrains[i].player == _player)
+                    modelConstrains[i].colorSet = _color;
+            }
+        }
+
+        public List<ModelConstrain> GetConstrains(Player _player)
+        {
+            List<ModelConstrain> constrains = new List<ModelConstrain>();
+            for (int i = 0; i < modelConstrains.Count; i++)
+            {
+                if (modelConstrains[i].player != _player)
+                    constrains.Add(modelConstrains[i]);
+            }
+            return constrains;
+        }
+
+        void InitConstrains(List<Player> _playerList)
+        {
+            for (int i = 0; i < _playerList.Count; i++)
+                modelConstrains.Add(new ModelConstrain(_playerList[i]));
+
+            for (int i = 0; i < modelConstrains.Count && i < AvatarDatas.Count; i++)
+            {
+                if (modelConstrains[i].avatarData == null)
+                    modelConstrains[i].avatarData = AvatarDatas[i];
+
+                if (modelConstrains[i].colorSet == null)
+                    modelConstrains[i].colorSet = AvatarDatas[i].ColorSets[i];
+            }
         }
 
         /// <summary>
@@ -33,6 +80,18 @@ namespace BlackFox
         {
             foreach (AvatarData data in LoadAvatarDatas())
                 AvatarDatas.Add(Instantiate(data));
+        }
+    }
+
+    public class ModelConstrain
+    {
+        public Player player;
+        public AvatarData avatarData;
+        public ColorSetData colorSet;
+
+        public ModelConstrain(Player _player)
+        {
+            player = _player;
         }
     }
 }

ff7ef9ede54c15d07fc0cf93a9f897163f2d0a3f
DESKTOP-B9NGNNA\Brago <bragolab@gmail.com>
2017-05-25 18:03:35 +0200

    - Pool manager.

---
+++ Assets/Scripts/PoolManager/PoolManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PoolManager {
+
+    public IPoollableObject ObjectPrefab;
+    public int MaxObjectInstanciated = 10;
+
+    List<IPoollableObject> Pool = new List<IPoollableObject>();
+    Transform container;
+
+    public PoolManager(Transform _container, IPoollableObject objectToInstantiate, int maxObjectInstanciated) {
+        container = _container;
+        ObjectPrefab = objectToInstantiate;
+        MaxObjectInstanciated = maxObjectInstanciated;
+        for (int i = 0; i < MaxObjectInstanciated; i++) {
+            createNewObject();
+        }
+    }
+
+    void createNewObject() {
+        IPoollableObject newObject = GameObject.Instantiate(ObjectPrefab.GameObject, container).GetComponent<IPoollableObject>();
+        newObject.poolManager = this;
+        Pool.Add(newObject);
+    }
+
+    #region API
+
+    public IPoollableObject GetPooledObject() {
+        if (Pool.FindAll(o => o.IsActive == false).Count < 1) {
+            createNewObject();
+        }
+
+        
+        for (int i = 0; i < Pool.Count; i++) {
+            if (Pool[i].IsActive == false) {
+                Pool[i].Activate();
+                return Pool[i];
+            }
+        }
+        return null;
+    }
+
+    public void ReleasedPooledObject(IPoollableObject objectToRelease) {
+        for (int i = 0; i < Pool.Count; i++) {
+            if (!Pool[i].IsActive) {
+                if (objectToRelease.Equals(Pool[i])) {
+                    //Pool[i].Deactivate();
+                    break;
+                }
+            }
+        }
+    }
+
+    #endregion
+
+
+}
---
+++ Assets/Scripts/PoolManager/PoolManagerTester.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManagerTester : MonoBehaviour {
+
+    PoolManager poolManager;
+    public GameObject ObjectToInstantiate;
+
+
+	// Use this for initialization
+	void Start () {
+        IPoollableObject prefab = ObjectToInstantiate.GetComponent<IPoollableObject>();
+        poolManager = new PoolManager(transform, prefab, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            IPoollableObject obj = poolManager.GetPooledObject() ;
+            obj.GameObject.transform.position = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), Random.Range(-3, 3));
+        }	
+	}
+
+
+}

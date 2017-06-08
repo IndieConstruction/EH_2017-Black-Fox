a1c305b5d25a3e41c4b208a9ed1404aec3b37184
FlyingBoar <montecristo310@gmail.com>
2017-06-08 15:09:38 +0200

    Ottimizzato Loading Panel

--- Assets/Scripts/UIManagers/UIControllers/LoadingController.cs
+++ Assets/Scripts/UIManagers/UIControllers/LoadingController.cs
@@ -8,21 +8,22 @@ namespace BlackFox {
     public class LoadingController : MonoBehaviour {
 
         public Image panelImg;
-        
+        public float FadeInTime = .4f;
+        public float FadeOutTime = .3f;
         #region API
 
         public void ActivateLoadingPanel(TweenCallback _action = null) {
             if (_action != null)
-                panelImg.DOFade(1, .3f).OnComplete(_action);
+                panelImg.DOFade(1.2f, FadeInTime).OnComplete(_action);
             else
-                panelImg.DOFade(1, 0.3f);
+                panelImg.DOFade(1.2f, FadeInTime);
         }
 
         public void DeactivateLoadingPanel(TweenCallback _action = null) {
             if (_action != null)
-                panelImg.DOFade(0, .3f).OnComplete(_action);
+                panelImg.DOFade(0, FadeOutTime).OnComplete(_action);
             else
-                panelImg.DOFade(0, .3f);
+                panelImg.DOFade(0, FadeOutTime);
         }
 
         #endregion

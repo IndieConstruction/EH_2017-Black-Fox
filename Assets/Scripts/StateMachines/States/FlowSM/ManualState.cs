a8fe3fa243cdf89b1c95041123c02ef589a65984
FlyingBoar <montecristo310@gmail.com>
2017-06-22 09:58:20 +0200

    - Bozza manual state

---
+++ Assets/Scripts/StateMachines/States/FlowSM/ManualState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackFox
+{
+    public class ManualState : StateBase
+    {
+        public override void OnStart()
+        {
+
+        }
+    }
+}

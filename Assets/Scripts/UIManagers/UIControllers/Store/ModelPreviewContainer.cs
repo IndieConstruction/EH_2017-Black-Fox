b2d363b6e46e2cc3dab7307d9d18fc1ec0a65377
RobbyBoxers <robertolarde@gmail.com>
2017-06-23 12:41:39 +0200

    -completati movimenti nello shop

---
+++ Assets/Scripts/UIManagers/UIControllers/Store/ModelPreviewContainer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+namespace BlackFox {
+    public class ModelPreviewContainer : BaseMenu {
+
+        List<RawImage> Images = new List<RawImage>();
+        StoreController storeController;
+        int _currentIndexSelected;
+        
+        int CurrentIndexSelected
+        {
+            get { return _currentIndexSelected; }
+            set
+            {
+                _currentIndexSelected = value;
+                if (_currentIndexSelected > 4 - 1)
+                    _currentIndexSelected = 0;
+
+                if (_currentIndexSelected < 0)
+                    _currentIndexSelected = 4 - 1;
+            }
+        }
+
+        public void Init(StoreController _controller)
+        {
+            storeController = _controller;
+            Images = GetComponentsInChildren<RawImage>().ToList();
+        }
+
+        public override void GoUpInMenu(Player _player)
+        {
+            CurrentIndexSelected--;
+            storeController.MoveActiveImage(Images[CurrentIndexSelected].rectTransform);
+        }
+
+        public override void GoDownInMenu(Player _player)
+        {
+            CurrentIndexSelected++;
+            storeController.MoveActiveImage(Images[CurrentIndexSelected].rectTransform);
+        }
+
+        public override void Selection(Player _player)
+        {
+         
+        }
+
+        public override void GoBack(Player _player)
+        {
+         
+        }
+    }
+}
---
+++ Assets/Scripts/UIManagers/UIControllers/Store/SkinPreviewContainer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+namespace BlackFox {
+
+public class SkinPreviewContainer : BaseMenu
+    {
+        List<Image> Images = new List<Image>();
+        StoreController storeController;
+
+        int _currentIndexSelected;
+
+        int CurrentIndexSelected
+        {
+            get { return _currentIndexSelected; }
+            set
+            {
+                _currentIndexSelected = value;
+                if (_currentIndexSelected > 4 - 1)
+                    _currentIndexSelected = 0;
+
+                if (_currentIndexSelected < 0)
+                    _currentIndexSelected = 4 - 1;
+            }
+        }
+
+        public void Init(StoreController _controller)
+        {
+            storeController = _controller;
+            Images = GetComponentsInChildren<Image>().ToList();
+            Images.Remove(Images[Images.Count - 1]);
+        }
+
+        #region Menu Action
+
+        public override void GoUpInMenu(Player _player)
+        {
+            CurrentIndexSelected--;
+            storeController.MoveActiveImage(Images[CurrentIndexSelected].rectTransform);
+        }
+
+        public override void GoDownInMenu(Player _player)
+        {
+            CurrentIndexSelected++;
+            storeController.MoveActiveImage(Images[CurrentIndexSelected].rectTransform);
+        }
+
+        public override void Selection(Player _player)
+        {
+            base.Selection(_player);
+        }
+
+        public override void GoBack(Player _player)
+        {
+            base.GoBack(_player);
+        }
+
+        #endregion
+
+        
+
+    }
+}
